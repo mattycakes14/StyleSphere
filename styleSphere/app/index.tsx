98973ca918d87f6d697261e49305b4361b0b1ad2
MatthewLau14 <mlau191@uw.edu>
2024-10-29 18:01:56 -0700

    test

--- styleSphere/app/index.tsx
+++ styleSphere/app/index.tsx
@@ -3,10 +3,7 @@ import { Text, View } from "react-native";
 export default function Page() {
   return (
     <View>
-      <View>
-        <Text>Hello World</Text>
-        <Text>This is the first page of your app.</Text>
-      </View>
+      <Text>Main Page</Text>
     </View>
   );
 }
