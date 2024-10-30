d0eadcbc3495cb91bc1300c44179e0255f03eedc
MatthewLau14 <mlau191@uw.edu>
2024-10-30 14:15:16 -0700

    Matt got that jimmy

--- styleSphere/app/index.tsx
+++ styleSphere/app/index.tsx
@@ -1,6 +1,32 @@
-import { Text, View } from "react-native";
-import Components from "@/components/Components";
+import React from "react";
+import { View, Image, Text, Button } from "react-native";
+import { useState } from "react";
+type hooperNames = {
+  name: string;
+};
 
-export default function Page() {
-  return <View>caleb is here</View>;
-}
+const index = () => {
+  return (
+    <View>
+      <Hoopers name="matt"></Hoopers>
+    </View>
+  );
+};
+
+const Hoopers = (props: hooperNames) => {
+  const [shotsMade, changeShots] = useState(0);
+
+  const incrementShots = () => {
+    changeShots(shotsMade + 1);
+  };
+  return (
+    <View>
+      <Text>State tracker for {props.name}</Text>
+      <Button onPress={incrementShots} title="Press IF he made it"></Button>
+      <Text>
+        {props.name} currently has made {shotsMade} number of shots
+      </Text>
+    </View>
+  );
+};
+export default index;
