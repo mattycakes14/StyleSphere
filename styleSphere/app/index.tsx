375a112b7e41287e016ff66152d77470f52c4926
MatthewLau14 <mlau191@uw.edu>
2024-10-31 17:26:49 -0700

    SWIPING FEATURES AND BASE TEMPLATE

--- styleSphere/app/index.tsx
+++ styleSphere/app/index.tsx
@@ -1,12 +1,41 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
+
+//child component imports
+import Profiles from "@/components/Profiles";
+import FilterBar from "@/components/FilterBar";
+import NavBar from "@/components/NavBar";
 
 const Index = () => {
   return (
-    <View>
-      <Text>Done</Text>
+    <View style={styles.container}>
+      <View style={styles.filterContainer}>
+        <FilterBar></FilterBar>
+      </View>
+      <View style={styles.profileContainer}>
+        <Profiles></Profiles>
+      </View>
+      <View style={styles.navBarContainer}>
+        <NavBar></NavBar>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+  },
+  profileContainer: {
+    flex: 1, //takes up remain space after navBar and filter bar are set
+  },
+  filterContainer: {
+    height: 60,
+  },
+  navBarContainer: {
+    height: 60,
+  },
+});
 
 export default Index;
---
+++ styleSphere/components/NavBar.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const NavBar = () => {
+  return (
+    <View>
+      <Text>NavBar</Text>
+    </View>
+  );
+};
+
+export default NavBar;
